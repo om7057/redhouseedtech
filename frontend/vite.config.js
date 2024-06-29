3dd0cbc0a6231d1f6079d2264a4ff7105ff1ac5b
om7057 <kulkarniom7057@gmail.com>
2024-06-29 11:57:22 +0530

    cOmMit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // or any other directory name
+    outDir: 'dist', 
   },
 });
