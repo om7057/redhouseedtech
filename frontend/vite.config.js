b3c326b8113898b37cfbb6a5065fe5c71b384ffe
om7057 <kulkarniom7057@gmail.com>
2024-06-29 12:42:48 +0530

    Gamora

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'build', 
   },
-  server: {
-    fs: {
-      strict: false
-    }
-  }
 });
